b957cd469ea084c3fd75ea3f2d246486f25d4501
hkhider62 <khiderhoussame@gmail.com>
2024-03-22 22:32:03 +0100

    challenges

---
+++ new2.js
@@ -0,0 +1,34 @@
+// challenge 1
+function filterevennumbers(evenlist) {
+    return evenlist.filter(num => num % 2 ===0)
+}
+
+//challenge2
+const person ={
+    name :"houssame",
+    famillyname :"khider",
+    age: 19,
+    email: "khiderhoussame@gmail.com",
+};
+//*the fonction that contains only the people who are over 30 years old.
+function filterpeopleover30(people){
+    return people.filter(person => person.age >30);
+}
+
+//challenge3
+let stack = [exemple];
+exemple.push(22);
+exemple.push(55);
+exemple.push(amin);
+
+//challenge4
+let queues =[exemple];
+exemple.push(ahamd);
+exemple.push(yassin);
+exemple.push(yousra);
+//challenge5
+function removeVowels(outvw) {
+    const Vowels = /[aeiouAEIOU]/g;
+    return outvw.replace(Vowels,'')
+}
+//challenge6
